0331703eddf905a05ca13275da3ed3a94043d691
Noah Yasskin <noahyasskin@gmail.com>
2016-11-02 04:31:25 +0000

    Implement display of message when no article present

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
   
   def new
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
         flash[:success] = "Article has been created"
         redirect_to articles_path
       else
-        flash[:danger] = "Article has not been created"
+        flash.now[:danger] = "Article has not been created"
         render :new
       end
   end
---
+++ spec/features/listing_article_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.feature "Listing Articles" do
+  
+  before do
+    @article1 = Article.create(title: "The first article", body: "Lorem Ipsum Dolor Sit Amet, Consectetur.")
+    @article2 = Article.create(title: "The second article", body: "Body of 2nd Article")
+  end
+  
+  scenario "A user lists all articles" do
+    visit "/"
+    
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    
+  end
+  
+  scenario "A user has no articles" do
+    Article.delete_all
+    
+    visit "/"
+    
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+    
+    within("h1#no-articles") do
+      expect(page).to have_content("No Articles Created")
+    end
+    
+  end
+
+end

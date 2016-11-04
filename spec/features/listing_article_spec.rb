6f9f4cd64742450c85e9392af30cb9263619dc61
Noah Yasskin <noahyasskin@gmail.com>
2016-11-04 04:27:53 +0000

    show new article button to signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.feature "Listing Articles" do
   
   before do
-    john = User.create(email: "jonh@example.com", password: "Password")
-    @article1 = Article.create(title: "The first article", body: "Lorem Ipsum Dolor Sit Amet, Consectetur.", user: john)
-    @article2 = Article.create(title: "The second article", body: "Body of 2nd Article", user: john)
+    @john = User.create(email: "jonh@example.com", password: "Password")
+    @article1 = Article.create(title: "The first article", body: "Lorem Ipsum Dolor Sit Amet, Consectetur.", user: @john)
+    @article2 = Article.create(title: "The second article", body: "Body of 2nd Article", user: @john)
   end
   
-  scenario "A user lists all articles" do
+  scenario "with articles created and user not signed in" do
     visit "/"
     
     expect(page).to have_content(@article1.title)
@@ -17,7 +17,22 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
     
+  end
+  
+   scenario "with article create and user signed in" do
+    login_as(@john)
+    visit "/"
+    
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+
   end
   
   scenario "A user has no articles" do

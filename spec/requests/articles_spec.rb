df06b86cc475613ce907f199d77cd1b9fbfc8a06
Noah Yasskin <noahyasskin@gmail.com>
2016-11-04 05:01:42 +0000

    add restricting access with controller actions

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,10 +3,49 @@ require "rails_helper"
 RSpec.describe "Articles", type: :request do
   
   before do
-    @article = Article.create(title: "Title One", body: "Body of article one")
+    @john = User.create(email: "john@example.com", password: "Password")
+    @fred = User.create(email: "fred@example.com", password: "Password")
+    @article = Article.create!(title: "Title One", body: "Body of article one", user: @john)
   end
   
-    describe 'Get /articles/:id' do
+    describe 'GET /articles/:id/edit' do
+      context 'with non-signed in user'do
+        before { get "/articles/#{@article.id}/edit" }
+
+        it "redirects to the signin page" do
+          expect(response.status).to eq 302
+          flash_message = "You need to sign in or sign up before continuing."
+          expect(flash[:alert]).to eq flash_message
+        end
+      end
+      
+      context 'with signed in user who is non-owner' do
+        before do
+          login_as(@fred)
+          get "/articles/#{@article.id}/edit"
+        end
+        
+        it "redirect to the homepage" do
+          expect(response.status).to eq 302
+          flash_message = "You can only edit your own article."
+          expect(flash[:alert]).to eq flash_message
+        end
+      end
+    end
+  
+    context 'with signed in user as owner successful edit' do
+      before do
+        login_as(@john)
+        get "/articles/#{@article.id}/edit"
+      end
+      
+      it "successfully edits article" do
+        expect(response.status).to eq 200
+      end
+      
+    end
+  
+    describe 'GET /articles/:id' do
       context 'with existing article' do
         before { get "/articles/#{@article.id}" }
       
